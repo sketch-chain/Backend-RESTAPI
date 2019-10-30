5ea409f86840b12e6cd011a8130f199d2261c153
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2019-10-30 14:47:53 +0900

    Create project

---
+++ src/main/java/com/sketchchain/sketchchain/SketchChainApplication.java
@@ -0,0 +1,13 @@
+package com.sketchchain.sketchchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SketchChainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sketchchain/sketchchain/SketchChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sketchchain.sketchchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
