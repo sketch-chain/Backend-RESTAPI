12a10b0fb8f2ba6556a7548960e0b5198f69a0a4
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2019-11-08 13:19:24 +0900

    Update RoomRepository

--- src/main/java/com/sketchchain/sketchchain/repository/RoomRepository.java
+++ src/main/java/com/sketchchain/sketchchain/repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.sketchchain.sketchchain.repository;
 
 import com.sketchchain.sketchchain.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 }
